805d3f0fe4817005e003684c91babe8fc2f2a1f8
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-14 20:30:38 +0530

    Update bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -1,13 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "s3backend"
   acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
 }
