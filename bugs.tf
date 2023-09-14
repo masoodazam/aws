de218250e2259c60ad8e591f2851ef0d6b1060dd
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-14 21:19:12 +0530

    Update bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "s3backend"
-    acl = "private"
+resource "aws_s3_bucket" "httpds3" {
+bucket = "raktim-hsttpd-files"
+acl    = "public-read"
 }
