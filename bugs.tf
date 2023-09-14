5a7235d76c480791e0ba0c322f1552c2fede1147
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-14 20:03:15 +0530

    bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "s3backend"
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
   acl    = "private"
 }
