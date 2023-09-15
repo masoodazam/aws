8c5b86701f22d326805c2ffaec9f4248e5cfebe2
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-15 12:06:03 +0530

    Update bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -1,12 +1,13 @@
-resource "aws_ec2" "My-ec2" {
-ami = "0f844a9675b22ea32"
-instance_type = "t2.micro"
-tags = {
-Name = "ProjectA-webserver"
-}
-lifecycle {
-ignore_changes = [
- tags,ami
-]
-}
+resource "aws_instance" "My-ec2" {
+  ami           = "0f844a9675b22ea32"  
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ProjectA-webserver"
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+      ami
+    ]
+  }
 }
