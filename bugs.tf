10a3b56ce1ea7bfbad32ee4b15ff5886ca6c7660
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-15 11:18:51 +0530

    Update bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_s3_bucket_acl" "httpds3_acl" {
+resource "aws_s3_bucket" "httpds3_acl" {
   bucket = aws_s3_bucket.httpds3.id
   acl    = "public-read"
 }
