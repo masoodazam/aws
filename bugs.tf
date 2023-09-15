0e133a8c4a599cdce13fa6d2b502c8680e54d601
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-15 11:21:23 +0530

    Update bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -1,9 +1,13 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "httpds3_acl" {
-  bucket = aws_s3_bucket.httpds3.id
+resource "aws_s3_bucket" "httpds3" {
+  bucket = "raktim-hsttpd-files"
   acl    = "public-read"
 }
 
+resource "aws_s3_bucket_acl" "httpds3_acl" {
+  bucket = aws_s3_bucket.httpds3.id
+  acl    = "public-read"
+}
