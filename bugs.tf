181a178f24a95de361a800b24a24a38fb69633f2
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-15 11:06:54 +0530

    bugs.tf

--- bugs.tf
+++ bugs.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_s3_bucket" "httpds3" {
-  bucket = "raktim-hsttpd-files"
+resource "aws_s3_bucket_acl" "httpds3_acl" {
+  bucket = aws_s3_bucket.httpds3.id
   acl    = "public-read"
 }
+
